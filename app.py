6101e99c07e7041e8af60c2f068f8e79102e046e
Tudor7441 <popovicitudor123@gmail.com>
2024-11-16 12:19:48 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -68,6 +68,7 @@ class Produse(db.Model):
     price = db.Column(db.Float, nullable=False)
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)  # Add email column
